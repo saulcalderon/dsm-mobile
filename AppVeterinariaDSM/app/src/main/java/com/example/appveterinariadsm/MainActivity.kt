bcc2c7a3ecafd587c5fe051bb8a3f8962eb1ba9c
BryanMena <bryanmena@outlook.com>
2024-05-03 11:36:51 -0600

    Splash e interfaces de acceso
    Se añade el Splash y las interfaces de inicio de sesión y registro.

---
+++ AppVeterinariaDSM/app/src/main/java/com/example/appveterinariadsm/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.appveterinariadsm
+
+import android.content.Intent
+import android.os.Bundle
+import android.webkit.WebView.FindListener
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AppVeterinariaDSM)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonIniciarSesion = this.findViewById<Button>(R.id.btnIniciarSesion)
+        buttonIniciarSesion.setOnClickListener{
+
+            val intent = Intent(this, InicioSesion:: class.java)
+            startActivity(intent)
+        }
+
+        val buttonRegistrarse = this.findViewById<Button>(R.id.btnRegistrarse)
+        buttonRegistrarse.setOnClickListener{
+
+            val intent = Intent(this, Registrarse:: class.java)
+            startActivity(intent)
+        }
+
+    }
+}
