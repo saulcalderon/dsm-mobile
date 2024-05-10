d73dcf3ddd748aefbe092a2bf33cc34d679d0c70
Miriam Villeda <102434406+miryv@users.noreply.github.com>
2024-05-10 16:20:30 -0600

    Arreglos

--- AppVeterinariaDSM/app/src/main/java/com/example/appveterinariadsm/MainActivity.kt
+++ AppVeterinariaDSM/app/src/main/java/com/example/appveterinariadsm/MainActivity.kt
@@ -35,66 +35,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Registrarse:: class.java)
             startActivity(intent)
         }
-
-        // Inicializar la base de datos Firebase
-        database = FirebaseDatabase.getInstance().reference.child("pacientes")
-        pacientesList = mutableListOf()
-
-        / Configurar el botón "Guardar"
-        btnGuardar.setOnClickListener {
-            guardarPacienteEnFirebase(
-                etNombre.text.toString(),
-                etEspecie.text.toString(),
-                etRaza.text.toString(),
-                etEdad.text.toString()
-            )
-            limpiarCampos()
-        }
-        // Configurar el botón "Ver"
-        btnVer.setOnClickListener {
-            mostrarPacientes()
-        }
-        // Escuchar cambios en la base de datos Firebase
-        database.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                pacientesList.clear()
-                for (data in snapshot.children) {
-                    val paciente = data.getValue(Paciente::class.java)
-                    paciente?.let { pacientesList.add(it) }
-                }
-            }
-
-
-    }
-}
-    private fun guardarPacienteEnFirebase(nombre: String, especie: String, raza: String, edad: String) {
-        val pacienteId = database.push().key
-        val paciente = Paciente(nombre, especie, raza, edad)
-        pacienteId?.let {
-            database.child(it).setValue(paciente)
-        }
-    }
-    private fun limpiarCampos() {
-        etNombre.text.clear()
-        etEspecie.text.clear()
-        etRaza.text.clear()
-        etEdad.text.clear()
-    }
-    private fun mostrarPacientes() {
-        val stringBuilder = StringBuilder()
-        for (paciente in pacientesList) {
-            stringBuilder.append("Nombre: ${paciente.nombre}\n")
-            stringBuilder.append("Especie: ${paciente.especie}\n")
-            stringBuilder.append("Raza: ${paciente.raza}\n")
-            stringBuilder.append("Edad: ${paciente.edad}\n\n")
-        }
-        tvInformacionPaciente.text = stringBuilder.toString()
     }
 }
 
-data class Paciente(
-    val nombre: String = "",
-    val especie: String = "",
-    val raza: String = "",
-    val edad: String = ""
-)
