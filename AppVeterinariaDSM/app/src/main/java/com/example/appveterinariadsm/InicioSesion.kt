21d1c6f093d140957e7f6778c184507d17426080
Andrea Elías <54760483+Andrea-EH@users.noreply.github.com>
2024-05-12 13:00:10 -0600

    InicioSesion.kt

--- AppVeterinariaDSM/app/src/main/java/com/example/appveterinariadsm/InicioSesion.kt
+++ AppVeterinariaDSM/app/src/main/java/com/example/appveterinariadsm/InicioSesion.kt
@@ -3,7 +3,9 @@ package com.example.appveterinariadsm
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,12 +21,44 @@ class InicioSesion : AppCompatActivity() {
             insets
         }
 
-        val buttonIS = this.findViewById<Button>(R.id.btnIniciarSesionS)
-        buttonIS.setOnClickListener{
+        val loginButton = this.findViewById<Button>(R.id.btnIniciarSesionS)
+        val usernameEditText = this.findViewById<EditText>(R.id.etUsuario)
+        val passwordEditText = this.findViewById<EditText>(R.id.etPassword)
+        loginButton.setOnClickListener{
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
 
-            val intent = Intent(this, Pacientes:: class.java)
-            startActivity(intent)
-        }
+            if (username == "Admin" && password == "123456"){
+                val builder = AlertDialog.Builder(this)
+
+                builder.setTitle("Mensaje")
+                builder.setMessage("Bienvenido Admin")
+
+                builder.setPositiveButton("OK") { dialog, which ->
+
+                    dialog.dismiss()
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+
+                val intent = Intent(this, Pacientes:: class.java)
+                startActivity(intent)
+
+            } else{
+                val builder = AlertDialog.Builder(this)
+
+                builder.setTitle("Mensaje")
+                builder.setMessage("Usuario y/o contraseña incorrectos")
+
+                builder.setPositiveButton("OK") { dialog, which ->
+
+                    dialog.dismiss()
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+            }
 
     }
 }
